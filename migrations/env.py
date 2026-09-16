84cbec5580f812e4882442182fbf7a8389891980
Terminal S16 <pavluchezo@gmail.com>
2026-09-16 20:07:57 +0300

    Add Dockerfile and fix alembic to use DB_PATH from settings

--- migrations/env.py
+++ migrations/env.py
@@ -5,9 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+from bot.config import settings
+
+config.set_main_option("sqlalchemy.url", f"sqlite:///{settings.db_path}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
